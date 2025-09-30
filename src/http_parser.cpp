5aabd242df29c6b3d1727d4d285fb73cc3df128e
RockonDreaming <60734273+aaaa0ggMC@users.noreply.github.com>
2025-09-30 19:38:13 +0800

    perf: Boost HTTPResponse::generate
    - Now HTTPResponse::generate() runs 2 times faster than before

--- src/http_parser.cpp
+++ src/http_parser.cpp
@@ -1,5 +1,6 @@
 #include <http_parser.h>
 #include <gtest/gtest.h>
+#include <alib-g3/aclock.h>
 // #include <format>
 
 using namespace mnginx;
@@ -9,35 +10,56 @@ void HTTPResponse::parse(std::string_view data){
 
 }
 
+/** perf
+ * @before 6593.11ms for 1'000'000 times ==> 6.59311us /call
+ * @after(current) 3217.43ms for 1'000'000  times ==> 3.21743us /call
+ */
 std::pmr::vector<char> HTTPResponse::generate(){
-    std::pmr::string pre_data;
-    pre_data.resize(2048);
-    //// Generate HTTP Version
-    if(version.minor)pre_data = std::format("HTTP/{}.{} {} {}\r\n",
-            version.major,version.minor,static_cast<int32_t>(status_code),status_str.c_str()
-        );
-    else pre_data = std::format("HTTP/{} {} {}\r\n",
-            version.major,static_cast<int32_t>(status_code),status_str.c_str()
-        );
-    
-    for(auto&[key,val] : headers){
-        pre_data += key;
-        // @todo @note we add a space after ':'
-        pre_data += ": ";
-        pre_data += val;
-        pre_data += "\r\n";
+    using data_t = std::pmr::vector<char>;
+    data_t rdata;
+    rdata.reserve(2048);
+
+    // used for small numbers,or frankly speaking,for HTTP Version code
+    static auto append_num = [](data_t & vec,int num){
+        if(num < 10){
+            vec.push_back('0' + num);
+        }else if(num < 100){
+            vec.push_back('0' + (num/10) % 10);
+            vec.push_back('0' + num % 10);
+        }else if(num < 1000){
+            vec.push_back('0' + num/100 % 10);
+            vec.push_back('0' + num/10 % 10);
+            vec.push_back('0' + num % 10);
+        }else{
+            std::cout << "Well,you win" << std::endl;
+        }// status code < 1000
+    };
+
+    rdata.insert(rdata.end(),{'H','T','T','P','/'});
+    append_num(rdata,version.major);
+    if(version.minor){
+        rdata.push_back('.');
+        append_num(rdata,version.major);
     }
-    pre_data += "\r\n";
+    rdata.push_back(' ');
+    append_num(rdata,static_cast<int32_t>(status_code));
+    rdata.push_back(' ');
+    rdata.insert(rdata.end(),status_str.begin(),status_str.end());
+    rdata.insert(rdata.end(),{'\r','\n'});
+
+    for(auto&[key,val] : headers){
+        rdata.insert(rdata.end(),key.begin(),key.end());
+        rdata.insert(rdata.end(),{':',' '});
+        rdata.insert(rdata.end(),val.begin(),val.end());
+        rdata.insert(rdata.end(),{'\r','\n'});
+    }
+    rdata.insert(rdata.end(),{'\r','\n'});
     
     if(!!data){
-        std::pmr::vector<char> rdata(pre_data.begin(),pre_data.end());
         HTTPData & d = *data;
-
         rdata.insert(rdata.end(),d.begin(),d.end());
-        return rdata;
-    }else{
-        return std::pmr::vector<char>(pre_data.begin(),pre_data.end());
     }
+    return rdata;
 }
 
 void HTTPRequest::parse(std::string_view data){
@@ -69,12 +91,22 @@ TEST(HTTPResponse, generate){
 
     EXPECT_FALSE(generated_response.empty());
     std::string response_str(generated_response.begin(), generated_response.end());
+    std::cout << alib::g3::Util::str_unescape(response_str) << std::endl;
     EXPECT_TRUE(response_str.find("HTTP/1.1 200 OK") != std::string::npos);
     EXPECT_TRUE(response_str.find("Content-Type: text/html") != std::string::npos);
     EXPECT_TRUE(response_str.find("Content-Length: ") != std::string::npos);
     if(resp.data){
         EXPECT_TRUE(response_str.find("Hello") != std::string::npos);
     }
+    
+    /*
+    std::cout << "Let's generate http request for 1,000,000 times!" << std::endl;
+    alib::g3::Clock clk;
+    for(int i = 0;i < 1'000'000;++i){
+        resp.generate();
+    }
+    std::cout << "It costs " << clk.getOffset() << "ms" << std::endl;
+    */
 }
 
 /* This test result varies in differect architecture,so I commented it.
